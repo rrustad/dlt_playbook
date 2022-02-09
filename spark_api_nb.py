a831dcf32ea8e3658c4b9a618316b18ec543e3ca
riley.rustad@databricks.com <riley.rustad@databricks.com>
2022-02-09 19:42:30 +0000

    dlt

--- spark_api_nb.py
+++ spark_api_nb.py
@@ -1,8 +1,10 @@
 # Databricks notebook source
 import pyspark.sql.functions as F
+import dlt
 
 # COMMAND ----------
 
+@dlt.table()
 def population_data():
   return (spark.readStream.format('cloudFiles')
   .option('cloudFiles.format', 'csv')
@@ -12,23 +14,20 @@ def population_data():
 
 # COMMAND ----------
 
-(population_data().writeStream.format('delta')
-  .option('checkpointLocation', '/tmp/dlt/population_data_bz/_checkpoints')
-  .trigger(once=True)
-  .table("riley_test.population_data"))
+# (population_data().writeStream.format('delta')
+#   .option('checkpointLocation', '/tmp/dlt/population_data_bz/_checkpoints')
+#   .trigger(once=True)
+#   .table("riley_test.population_data"))
 
 # COMMAND ----------
 
+@dlt.table()
 def population_agg():
-  return(spark.table("riley_test.population_data")
+  return(spark.table("LIVE.population_data")
   .groupby('year')
   .agg(F.sum('population').alias("total_population")))
 
 
 # COMMAND ----------
 
-population_agg().write.format('delta').mode("overwrite").saveAsTable('population_agg')
-
-# COMMAND ----------
-
-
+# population_agg().write.format('delta').mode("overwrite").saveAsTable('population_agg')
