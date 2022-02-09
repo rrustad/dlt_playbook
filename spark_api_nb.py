fd92061633b5744689f1d81d3110f18d2a58d70a
riley.rustad@databricks.com <riley.rustad@databricks.com>
2022-02-09 17:39:44 +0000

    refactor to functions

--- spark_api_nb.py
+++ spark_api_nb.py
@@ -9,15 +9,16 @@ bz_checkpoint_path = '/tmp/dlt/population_data_bz/_checkpoints'
 
 # COMMAND ----------
 
-df = (spark.readStream.format('cloudFiles')
-  .option('cloudFiles.format', 'csv')
-  .option('header', 'true')
-  .schema('city string, year int, population long')
-  .load(source_path))
+def extract_data():
+  return (spark.readStream.format('cloudFiles')
+    .option('cloudFiles.format', 'csv')
+    .option('header', 'true')
+    .schema('city string, year int, population long')
+    .load(source_path))
 
 # COMMAND ----------
 
-(df.writeStream.format('delta')
+(extract_data().writeStream.format('delta')
   .option('checkpointLocation', bz_checkpoint_path)
   .trigger(once=True)
   .start(bz_write_path))
@@ -29,11 +30,12 @@ sv_checkpoint_path = '/tmp/dlt/population_data_sv/sv_checkpoints'
 
 # COMMAND ----------
 
-sv_df = (spark.read.format('delta')
-  .load(bz_write_path)
-  .groupby('year')
-  .agg(F.sum('population').alias("total_population")))
+def transfrom_data():
+  return (spark.read.format('delta')
+    .load(bz_write_path)
+    .groupby('year')
+    .agg(F.sum('population').alias("total_population")))
 
 # COMMAND ----------
 
-sv_df.write.format('delta').mode("overwrite").save(sv_write_path)
+transfrom_data().write.format('delta').mode("overwrite").save(sv_write_path)
